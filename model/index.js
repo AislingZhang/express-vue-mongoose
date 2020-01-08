45a26e9c66f1b55b0e12c2d01ee58211ac1f0d87
ZL <zhanglin2011@xinhua.org>
2020-01-08 20:45:28 +0800

    前后端数据库整合

---
+++ model/index.js
@@ -0,0 +1,6 @@
+const mongoose=require('mongoose');
+let BookSchema={name:String,price:Number};
+let Customer={name:String,password:String,score:Number};
+
+mongoose.model("Book",BookSchema);
+mongoose.model("Customer",Customer);
